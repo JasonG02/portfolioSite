fd7243d32b4ba082696fbb0e2d9af4db961324bb
Jason G <jasong@Jasons-MacBook-Air.local>
2024-04-17 19:37:48 -0500

    Update to website

--- script.js
+++ script.js
@@ -2,11 +2,50 @@ const menu = document.querySelector(".menu");
 const hidden = document.querySelector(".hidden");
 const sideBar = document.querySelector(".sidebar--container");
 const close = document.querySelector(".close");
-
-menu.addEventListener("click", function () {
+const sb = menu.addEventListener("click", function () {
   sideBar.classList.remove("hidden");
 });
 
 close.addEventListener("click", function () {
   sideBar.classList.add("hidden");
 });
+
+let typingEffect = new Typed(".typedText", {
+  strings: [" a Developer", "Jason"],
+  loop: true,
+  typeSpeed: 110,
+  backspeed: 110,
+  backdelay: 2000,
+});
+
+const srLeft = ScrollReveal({
+  origin: "left",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+const srRight = ScrollReveal({
+  origin: "right",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(".about-pic", { delay: 100 });
+srRight.reveal(".p-info", { delay: 100 });
+srRight.reveal(".box", {});
+srRight.reveal(".boxTwo", {});
+sr.reveal(".detail", {});
+sr.reveal(".learn", {});
+sr.reveal(".top-header", {});
+sr.reveal(".links", {});
+srRight.reveal(".proj", {});
+srRight.reveal(".proj-h1", {});
